94a70003a29beebcc90024ade81b266f8b83f965
beckyxz <105518655+beckyxz@users.noreply.github.com>
2022-05-20 16:29:32 +0000

    matricula

--- PEOO2022/Lista01/matricula.cs
+++
@@ -1,6 +0,0 @@
-using System;
-puplic class Program {
-public static void Main(string[] args)  {
-Console.WriteLine("20211011110038");
-}
-}
--- PEOO2022/Lista01/meu_nome.cs
+++
@@ -1,6 +0,0 @@
-using System;
-puplic class Program {
-public static void Main(string[] args)  {
-Console.WriteLine("Rebeca Andrielly da Silva Araujo"):
-}
-}
